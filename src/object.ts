7b61d6bca0304cf5050e66ce2cc644881b5bcace
Ethan Glover <hello@ethang.email>
2023-10-22 20:55:38 -0500

    Add object utilities and deep merge function
    Introduced new 'object.ts' containing utilities for handling objects, such as emptiness check and new deep merge function. These would enhance data management by allowing more efficient checking of empty states and merging complex objects.

---
+++ src/object.ts
@@ -0,0 +1,35 @@
+import { isNil } from './data';
+
+type ObjectType = Record<string, unknown>;
+
+type ComplexIntersection<A, B> = (A & B)[keyof A] & (A & B)[keyof B];
+
+export function isObject(item: unknown): item is ObjectType {
+  return !isNil(item) && typeof item === 'object' && !Array.isArray(item);
+}
+
+export function deepMerge<T extends ObjectType, U extends ObjectType>(
+  target: T,
+  source: U,
+): T & U {
+  const output = { ...target } as Partial<T & U>;
+
+  for (const key in source) {
+    if (!isNil(source[key])) {
+      const value = source[key as keyof typeof source];
+
+      if (isObject(value) && isObject(target[key])) {
+        // eslint-disable-next-line functional/immutable-data
+        output[key as keyof typeof output] = deepMerge(
+          target[key] as ObjectType,
+          value as ObjectType,
+        ) as ComplexIntersection<T, U>;
+      } else {
+        // eslint-disable-next-line functional/immutable-data
+        output[key as keyof typeof output] = value as ComplexIntersection<T, U>;
+      }
+    }
+  }
+
+  return output as T & U;
+}

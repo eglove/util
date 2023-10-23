8084ac186e1eeca79846fa6fa8eafb2ee0f3b1e5
Ethan Glover <hello@ethang.email>
2023-10-22 21:04:10 -0500

    Refactor deepMerge function to accept multiple sources
    Change is introduced to the deepMerge function in the 'object.ts' file to allow it to handle multiple sources. This update will facilitate the merging of thr complex and nested objects and improve the efficiency and flexibility of data operations. The logic of handling nil values and complex intersections is retained but wrapped into an additional loop through all provided sources.

--- src/object.ts
+++ src/object.ts
@@ -10,23 +10,28 @@ export function isObject(item: unknown): item is ObjectType {
 
 export function deepMerge<T extends ObjectType, U extends ObjectType>(
   target: T,
-  source: U,
+  ...sources: U[]
 ): T & U {
   const output = { ...target } as Partial<T & U>;
 
-  for (const key in source) {
-    if (!isNil(source[key])) {
-      const value = source[key as keyof typeof source];
+  for (const source of sources) {
+    for (const key in source) {
+      if (!isNil(source[key])) {
+        const value = source[key as keyof typeof source];
 
-      if (isObject(value) && isObject(target[key])) {
-        // eslint-disable-next-line functional/immutable-data
-        output[key as keyof typeof output] = deepMerge(
-          target[key] as ObjectType,
-          value as ObjectType,
-        ) as ComplexIntersection<T, U>;
-      } else {
-        // eslint-disable-next-line functional/immutable-data
-        output[key as keyof typeof output] = value as ComplexIntersection<T, U>;
+        if (isObject(value) && isObject(target[key])) {
+          // eslint-disable-next-line functional/immutable-data
+          output[key as keyof typeof output] = deepMerge(
+            target[key] as ObjectType,
+            value as ObjectType,
+          ) as ComplexIntersection<T, U>;
+        } else {
+          // eslint-disable-next-line functional/immutable-data
+          output[key as keyof typeof output] = value as ComplexIntersection<
+            T,
+            U
+          >;
+        }
       }
     }
   }

d056a90b3a2c6f5fdc6704ea5d34edf002ee3ab6
Ethan Glover <hello@ethang.email>
2023-11-04 17:54:22 -0500

    Add getCookieValue method to handle cookie retrieval
    Introduced the getCookieValue method in request.ts to streamline the extraction of cookie values from request headers. This method streamlines code reuse, centralizing in one location a common task that may be required in different parts of the application. This function retrieves the value of a cookie given its name or returns null if it doesn't exist.

---
+++ src/request.ts
@@ -0,0 +1,23 @@
+import { isNil } from './data';
+
+export function getCookieValue(
+  cookieName: string,
+  requestHeaders: Headers,
+): string | null {
+  const cookies = requestHeaders.get('Cookie');
+
+  if (isNil(cookies)) {
+    return null;
+  }
+
+  const cookieArray = cookies.split(';');
+  for (const cookie of cookieArray) {
+    const [name, value] = cookie.split('=');
+
+    if (name.trim() === cookieName.trim()) {
+      return value.trim();
+    }
+  }
+
+  return null;
+}

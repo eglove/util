f7210db9284e5b151137a55b8923b4faf2063732
Ethan Glover <hello@ethang.email>
2023-11-04 17:22:34 -0500

    Add formatDateForDatetimeLocalInput function to date.ts
    This function is used to format date variable into a string that can be directly used in an input of type 'datetime-local'. This function takes a Date object and returns a string with Year-Month-Day then 'T' and Hour:Minute format.

---
+++ src/date.ts
@@ -0,0 +1,9 @@
+export function formatDateForDatetimeLocalInput(date: Date): string {
+  const year = date.getFullYear();
+  const month = (1 + date.getMonth()).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minute = date.getMinutes().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day}T${hour}:${minute}`;
+}

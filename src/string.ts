a2db422b819e469a26c26365502d03a9b0529393
Ethan Glover <hello@ethang.email>
2023-11-18 14:49:54 -0600

    Refactor urlToCapitalizedWords function for detailed URL info
    The `urlToCapitalizedWords` function has been altered to now return a array of object, containing both the capitalized text and complete URL path in `pathLabel` and `pathUrl` respectively. This change allows the function to provide more detailed information, enhancing its utility in URL processing tasks throughout the application.

--- src/string.ts
+++ src/string.ts
@@ -1,23 +1,34 @@
 import { isEmpty } from './data';
 
-export function urlToCapitalizedWords(url: URL): string[] {
+type UrlDetail = {
+  pathLabel: string;
+  pathUrl: URL;
+};
+
+export function urlDetails(url: URL): UrlDetail[] {
   const paths = url.pathname.split('/').filter(item => {
     return !isEmpty(item);
   });
 
-  let words: string[] = [];
+  let words: UrlDetail[] = [];
+  const urlBuilder = new URL(url.origin);
 
   for (const path of paths) {
+    const url = `${urlBuilder.toString()}/${path}`;
+
     words = [
       ...words,
-      path
-        .split(/\W/g)
-        .map(word => {
-          return `${word.charAt(0).toUpperCase()}${word
-            .slice(1)
-            .toLowerCase()}`;
-        })
-        .join(' '),
+      {
+        pathLabel: path
+          .split(/\W/g)
+          .map(word => {
+            return `${word.charAt(0).toUpperCase()}${word
+              .slice(1)
+              .toLowerCase()}`;
+          })
+          .join(' '),
+        pathUrl: new URL(url),
+      },
     ];
   }
 

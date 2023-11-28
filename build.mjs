ad7223300c040e263dedb0ff909fc5e2e7ba6ab5
Ethan Glover <hello@ethang.email>
2023-11-27 19:21:44 -0600

    esm

--- build.mjs
+++ build.mjs
@@ -1,7 +1,6 @@
 import * as fs from 'node:fs'
 import { rimraf } from 'rimraf'
 import { execSync } from 'child_process'
-import * as esbuild from 'esbuild'
 
 await rimraf('dist');
 
@@ -11,13 +10,5 @@ fs.copyFileSync(
   'package.json',
   'dist/package.json',
 )
-
-esbuild.buildSync({
-  entryPoints: ['src/*'],
-  bundle: true,
-  outdir: 'dist',
-  minify: true,
-  treeShaking: true,
-})
 
 execSync('cd dist && npm publish --access public && cd ..')

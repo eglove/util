7c222222d0f0297d4edcfd2ef1b4d6a78b2c900d
Ethan Glover <hello@ethang.email>
2023-12-03 09:18:19 -0600

    Version Bump

---
+++ test/object.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+
+import { deepMerge } from '../src/object';
+
+describe('object', () => {
+  it('should deep merge objects', () => {
+    const tsConfig = {
+      compilerOptions: { emitDeclarationOnly: false, someOption: true },
+    };
+    const newObject = {
+      compilerOptions: {
+        emitDeclarationOnly: true,
+      },
+      include: ['src'],
+    };
+
+    const merged = deepMerge(tsConfig, newObject);
+    expect(merged).toStrictEqual({
+      compilerOptions: {
+        emitDeclarationOnly: true,
+        someOption: true,
+      },
+      include: ['src'],
+    });
+  });
+});
